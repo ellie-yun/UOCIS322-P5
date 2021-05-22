1c092aaee0f29a7b259b0d1b8bf16aa251f3f4a4
Yebin Yun <yebin@Yebins-MacBook-Air.local>
2021-05-21 22:01:09 -0700

    Factored out the db operations

--- brevets/db.py
+++ brevets/db.py
@@ -3,21 +3,25 @@ Database insertion and retrieval
 """
 from pymongo import MongoClient
 
-class database:
-    def __init__(self, db_name="testdb"):
-        self.db_name = db_name
+class Mongodb:
+    def __init__(self, client_name="testdb"):
+        self.client_name = client_name
         self.client = None
         self.db = None
 
     def connect(self):
-        self.client = MongoClient('mongodb://' + self.db_name, 27017)
-        self.db = self.client.brevetsdb
+        self.client = MongoClient('mongodb://' + self.client_name, 27017)
 
-    def insert(self, row):
-        self.db.latestsubmit.insert_one(row)
+    def set_db(self, db_name):
+        self.db = self.client[db_name]
 
-    def delete_all_rows(self):
-        self.db.latestsubmit.delete_many({})
+    def insert(self, collection, row):
+        self.db[collection].insert_one(row)
 
-    def list_all_rows(self):
-        return list(self.db.latestsubmit.find())
+    def delete_all_rows(self, collection):
+        self.db[collection].delete_many({})
+
+    def list_all_rows(self, collection):
+        return list(self.db[collection].find())
+
+
